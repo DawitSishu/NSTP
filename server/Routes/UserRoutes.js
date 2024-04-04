58b94898e9a6060cd1c7a0b667217af64279861b
Dave <you@example.com>
2024-04-04 16:39:59 +0300

    users

---
+++ server/Routes/UserRoutes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+//sign up
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ const app = require("express")();
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const connectToDb = require("./Config/ConnectDB");
+const errorHandler = require("./Middlewares/ErrorHandler");
 
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 connectToDb();
 
+app.use("/api/user", require("./Routes/UserRoutes"));
+
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
 });
