bd4832e9501508c73a3cb67980d19a65fdca2e47
Dave <you@example.com>
2024-04-04 16:33:11 +0300

    user model

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  name: { type: String },
+  phone: { type: String },
+  balance: { type: Number, default: 0 },
+  type: { type: String },
 });
 
 module.exports = mongoose.model("User", userSchema);
