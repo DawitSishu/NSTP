b4d2fd5b7dfb7d8904171658852c9c9fa3de73c0
Dave <you@example.com>
2024-04-04 16:11:27 +0300

    DB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const app = require("express")();
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const connectToDb = require("./Config/ConnectDB");
 
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+connectToDb();
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
