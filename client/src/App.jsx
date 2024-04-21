471834eb8bab0c036f7f22e22b569b034206fb7a
DawitSishu <dawitshishu@gmail.com>
2024-04-21 09:27:23 -0700

    firebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense, useState } from "react";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Login = React.lazy(() => import("./Pages/LogIn"));
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#05223b",
+    },
+    secondary: {
+      main: "#EEE2B1",
+    },
+    tertiary: {
+      main: "#FFFFFF",
+    },
+    background: {},
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          background:
+            "radial-gradient(circle at center center, rgba(46, 46, 46,0.05) 0%, rgba(46, 46, 46,0.05) 22%,rgba(136, 136, 136,0.05) 22%, rgba(136, 136, 136,0.05) 26%,rgba(106, 106, 106,0.05) 26%, rgba(106, 106, 106,0.05) 66%,rgba(196, 196, 196,0.05) 66%, rgba(196, 196, 196,0.05) 77%,rgba(166,166,166, 0.050980392156862744) 77%, rgba(166,166,166, 0.050980392156862744) 93%,rgb(57,48,49) 93%, rgb(57,48,49) 100%),radial-gradient(circle at center center, rgb(20,20,20) 0%, rgb(20,20,20) 8%,rgb(20,20,20) 8%, rgb(20,20,20) 46%,rgb(20,20,20) 46%, rgb(20,20,20) 60%,rgb(20,20,20) 60%, rgb(20,20,20) 80%,rgb(20,20,20) 80%, rgb(20,20,20) 82%,rgb(20,20,20) 82%, rgb(20,20,20) 100%); background-size: 43px 43px;",
+        },
+      },
+    },
+  },
+});
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<div>hi</div>} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { initializeApp } from "firebase/app";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const firebaseConfig = {
+  apiKey: "AIzaSyBuW5uTHVZFTTgBwkAEnFw5_E8RD3RCJOk",
+  authDomain: "jemaw-c6f46.firebaseapp.com",
+  projectId: "jemaw-c6f46",
+  storageBucket: "jemaw-c6f46.appspot.com",
+  messagingSenderId: "641640506424",
+  appId: "1:641640506424:web:f8b0d2a8d623f41442b2cd",
+};
+initializeApp(firebaseConfig);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
